283a9c8b3037d7d20474ee5c4baee5a063cf8934
LabKitty <labkittyx@gmail.com>
2014-10-16 10:20:54 +0300

    Add ÜL 4.3

--- layouts/exercises.php
+++ layouts/exercises.php
@@ -23,7 +23,11 @@
 				<img src="http://users.khk.ee/kaspar.naaber/svp/periood-1/arvestus/images/<?php echo rand(1,12);?>.jpg" alt="Random Image" />
 
 			<h2>Kõikide piltide väljastamine</h2>
-        <!--  Sisesta siia kood -->
+				<?php  "http://users.khk.ee/kaspar.naaber/svp/periood-1/arvestus/images/";
+					for ($image = 1; $image <= 12; $image ++) {
+						echo '<img src="http://users.khk.ee/kaspar.naaber/svp/periood-1/arvestus/images/'.$image.'.jpg" alt="Random Image" />';
+					}
+				?> 
 		</section>
     </main>
 
